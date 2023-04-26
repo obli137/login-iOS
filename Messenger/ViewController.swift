969be30099678ae8f1300971e332ea9e2009b0f3
obli137 <obli137@gmail.com>
2023-04-26 09:08:28 -0300

    First Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Mariano Obligado on 26/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
